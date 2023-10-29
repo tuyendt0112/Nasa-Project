a527ccd3d35235d580a0ef425d2f875f5f022d6a
tuyendt0112 <dangthanhtuyen0112@gmail.com>
2023-10-29 23:29:18 +0700

    update

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,26 @@
+const API_URL = "http://localhost:8000/planets";
+
+async function httpGetPlanets() {
+  // TODO: Once API is ready.
+  // Load planets and return as JSON.
+  const response = await fetch(API_URL);
+
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
